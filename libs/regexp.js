2e21123ecc0c77e278357b5fddf35dcae1b2493e
Norman Enmanuel <normancarcamo@gmail.com>
2015-06-15 04:39:41 -0600

    Delete regexp.js

--- libs/regexp.js
+++
@@ -1,6 +0,0 @@
-module.exports.type = {
-	email: /^([a-zA-Z\_\-\.]+[a-zA-Z\_\-\.0-9]*)\@([a-z]*)\.[a-z]+$/,
-	phone: /^([0-9\-\s]+[0-9\-\s]*)$/,
-	generic: /^([a-zA-Z\_\-\.]+[a-zA-Z\_\-\.0-9]*)$/,
-	jsfiles: /^([a-zA-Z0-9\.])+(js)$/
-}
